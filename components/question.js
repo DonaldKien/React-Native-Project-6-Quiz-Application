261da5357c263d968e66da6397391f7e0644190c
DonaldKien <60657226+DonaldKien@users.noreply.github.com>
2020-05-27 17:21:20 +0800

    fetch Api
    fetch Api,
    show api question,
    show api answer

--- components/question.js
+++ components/question.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Answer from './answer';
 
-export default function App() {
+export default class Question extends React.Component {
 
-    // https://opentdb.com/api.php?amount=10&category=17&difficulty=medium
-  return (
-    <View style={styles.container}>
-      <Text>Question</Text>
-    </View>
-  );
+  constructor() {
+    super();
+    this.state = {
+      questionJson: [],
+      questionNumber: 0
+    }
+  }
+
+  componentDidMount () {
+    fetch('https://opentdb.com/api.php?amount=10&category=17&difficulty=medium')
+    .then( response => response.json() )
+    .then ( responseJson => this.setState( {questionJson: responseJson["results"]} ) )
+    .catch ( error => console.log(error) ) 
+  } 
+    
+  render () {
+    if (this.state.questionJson.length > 0) {
+      let currentQuestion = this.state.questionJson[this.state.questionNumber];
+      let selection = currentQuestion["incorrect_answers"].concat(currentQuestion["correct_answer"]);
+      return (
+        <View style={styles.container}>
+          <Text>{currentQuestion["question"]}</Text>
+          {selection.map( val => <Answer select={val} /> )}
+        </View>
+      )
+    }
+    else {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+  }
+
 }
 
 const styles = StyleSheet.create({
